71f429367fc1a380e29ad5525e88043236a0ae6c
Matilde Alexandra Arévalo León <m.arevalo@nelumbo.com.co>
2026-09-05 23:52:50 -0500

    chore: initial project

---
+++ src/main/java/com/pragma/powerup/FoodCourtApplication.java
@@ -0,0 +1,11 @@
+package com.pragma.powerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCourtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCourtApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pragma/powerup/FoodCourtApplicationTest.java
@@ -0,0 +1,11 @@
+package com.pragma.powerup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class FoodCourtApplicationTest {
+    @Test
+    void applicationClassIsAvailable() {
+        assertThat(FoodCourtApplication.class).isNotNull();
+    }
+}
